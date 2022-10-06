63a9d4a69f6ac7441466e31d5689e3ee77f1cb37
Aleksandr Elizarov <akelizarovav@gmail.com>
2022-10-06 21:52:01 +0300

    исправил тесты Lion

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -7,55 +7,29 @@ import org.junit.runners.Parameterized;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.*;
 
-@RunWith(Parameterized.class)
+@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
 
-    private final String checkedSex;
-    private final boolean expectedHasMane;
-
-    public LionTest(String checkedSex, boolean expectedHasMane) {
-        this.checkedSex = checkedSex;
-        this.expectedHasMane = expectedHasMane;
-    }
-
-    @Parameterized.Parameters
-    public static Object[][] getLionData() {
-        return new Object[][]{
-                {"Самец", true},
-                {"Самка", false},
-        };
-    }
-    @Test
-    public void doesHaveManeForCorrectValueShowTrueOrFalse() throws Exception {
-        Feline feline = new Feline();
-        Lion lion = new Lion(checkedSex, feline);
-        boolean actualHasMane = lion.doesHaveMane();
-        assertEquals("Метод doesHaveMane() в классе Lion не может определить есть ли грива."
-                , expectedHasMane, actualHasMane);
-    }
-    @Before
-    public void setUp(){
-        MockitoAnnotations.openMocks(this);
-    }
     @Mock
     Feline feline;
 
     @Test
-    public void getKittens() throws Exception {
+    public void getKittensTest() throws Exception {
         Lion lion = new Lion("Самка", feline);
         Mockito.when(feline.getKittens()).thenReturn(1);
         assertEquals("Ошибка, метод возвращает не верное значение", 1, lion.getKittens());
     }
     @Test
-    public void getFood() throws Exception {
-        Feline feline = new Feline();
+    public void getFoodTest() throws Exception {
         Lion lion = new Lion("Самец", feline);
         List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.getFood("Хищник")).thenReturn(expectedFood);
         assertEquals("Списки не совпадают", expectedFood, lion.getFood());
     }
 }
